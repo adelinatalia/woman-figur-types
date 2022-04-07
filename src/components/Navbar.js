eacada1ef9c751a26c45f7ff70b9fa96c0e4127d
Natalia <null>
2022-04-07 10:02:32 +0200

    navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,9 +1,11 @@
 import React from 'react'
+import { NavLink } from 'react-router-dom'
 
 export default function Navbar() {
     return (
         <nav>
-
+            <NavLink to="/" activeclassname="active-link">Home</NavLink>
+            <NavLink to="/identifyShape" activeclassname="active-link">identify shape</NavLink>
         </nav>
     )
 }
