ad4428f64de421f3a8d9b68ad0fa90518a3e8692
Natalia <onataliadelina@gmail.com>
2022-04-07 12:58:26 +0200

    nav

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,11 +1,34 @@
 import React from 'react'
 import { NavLink } from 'react-router-dom'
+import hourglass from '../assets/icons/nav-hourglass.png'
+import triangle from '../assets/icons/nav-triangle.png'
+import invertedTriangle from '../assets/icons/nav-inverted-triangle.png'
+import oval from '../assets/icons/nav-oval.png'
+import rectangle from '../assets/icons/nav-rectangle.png'
+import '../style/navbar.css'
 
 export default function Navbar() {
     return (
         <nav>
             <NavLink to="/" activeclassname="active-link">Home</NavLink>
-            <NavLink to="/identifyShape" activeclassname="active-link">identify shape</NavLink>
+            <div className="nav-figures">
+                <NavLink to="/hourglass" activeclassname="active-link">
+                    <img src={hourglass} alt="hourglass" />
+                </NavLink>
+                <NavLink to="/triangle" activeclassname="active-link">
+                    <img src={triangle} alt="triangle" />
+                </NavLink>
+                <NavLink to="/invertedTriangle" activeclassname="active-link">
+                    <img src={invertedTriangle} alt="inverted triangle" />
+                </NavLink>
+                <NavLink to="/oval" activeclassname="active-link">
+                    <img src={oval} alt="" />
+                </NavLink>
+                <NavLink to="/rectangle" activeclassname="active-link">
+                    <img src={rectangle} alt="rectangle" />
+                </NavLink>
+            </div>
+            <NavLink to="/identifyShape" activeclassname="active-link">Identify shape</NavLink>
         </nav>
     )
 }
